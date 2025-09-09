8f1b6caa8396c3aebc72d54065a39153366f7dbf
ShadowOfCold <mdelizov@mail.ru>
2025-09-09 14:47:26 +0800

    Lab #1

---
+++ Lab #1/Lab #1.py
@@ -0,0 +1,29 @@
+# АБГВА
+# А: Google Chrome
+# Б: locked_out_user (standard_user) / secret_sauce
+# Г: Сортировка по цене (high to low)
+# В: Добавить самый дешевый товар (обратите внимание на сортировку!).
+# A: Добавить товар в корзину и завершить работу скрипта.
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+browser = webdriver.Chrome()
+browser.get("https://www.saucedemo.com/")
+
+username = browser.find_element(By.ID, "user-name")
+password = browser.find_element(By.ID, "password")
+username.send_keys("standard_user")
+password.send_keys("secret_sauce")
+browser.find_element(By.ID, "login-button").click()
+
+select = Select(browser.find_element(By.CLASS_NAME, "product_sort_container"))
+select.select_by_visible_text("Price (high to low)")
+time.sleep(1)
+
+browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+buttons = browser.find_elements(By.CLASS_NAME, "btn_inventory")
+buttons[-1].click()
+time.sleep(1)
