d7ce666017d5862f8d6f684135a8626ea9fce77e
ShadowOfCold <mdelizov@mail.ru>
2025-09-15 21:30:07 +0800

    username fix

--- Lab #1/Lab #1.py
+++ Lab #1/Lab #1.py
@@ -15,9 +15,14 @@ browser.get("https://www.saucedemo.com/")
 
 username = browser.find_element(By.ID, "user-name")
 password = browser.find_element(By.ID, "password")
-username.send_keys("standard_user")
+username.send_keys("locked_out_user")
 password.send_keys("secret_sauce")
 browser.find_element(By.ID, "login-button").click()
+error = browser.find_element(By.CSS_SELECTOR, "[data-test='error']")
+if "locked out" in error.text.lower():
+    username.clear()
+    username.send_keys("standard_user")
+    browser.find_element(By.ID, "login-button").click()
 
 select = Select(browser.find_element(By.CLASS_NAME, "product_sort_container"))
 select.select_by_visible_text("Price (high to low)")
